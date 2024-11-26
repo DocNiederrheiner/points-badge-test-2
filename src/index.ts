ff855adc330575f4c9fedd575f48d3b296d103e6
DocNiederrheiner <75276057+DocNiederrheiner@users.noreply.github.com>
2024-11-26 10:35:09 +0100

    Update index.ts
    Introduced statistics, user count, avg. solutionCount, Top5 Users and finding top badge in seperate function.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
+import { getAllUser } from './user-store';
 
 export const getUsersBadge = async ( user: User ): Promise<Icon> => {
     let result : Icon | null = null;
@@ -20,7 +21,39 @@ export const getUsersBadge = async ( user: User ): Promise<Icon> => {
 };
 
 function calculateUsersStatistics() {
-  // todo
+  getAllUser().then((users) => {
+    let avgSolutionCount = 0;
+    users = users.sort((a,b) => b.solutionCount-a.solutionCount);
+    let badges : Promise<Icon>[] = [];
+    users.forEach(async (user) => {
+      avgSolutionCount += user.solutionCount;
+      badges.push(getUsersBadge(user));
+    })
+    avgSolutionCount = avgSolutionCount/users.length;
+    // Print statistics in console
+    console.log("Total number of users: "+ users.length);
+    console.log("Average solution count: "+ avgSolutionCount);
+    console.log("Top 5 User:")
+    for(let i = 0; i<= 4; i++){
+      console.log((i+1)+". "+users[i].username+": "+users[i].solutionCount+" solutions");
+    }
+    Promise.all(badges).then((resolvedBadges) => console.log("The most given badge is "+ Icon[findTopBadge(resolvedBadges)]));
+  });
+
+  
 }
 
 calculateUsersStatistics();
+
+function findTopBadge(badges: Icon[]) : Icon {
+  let result : Icon = Icon.BADGE_BAD_ASS;
+  let maxcount : number = 0;
+  Object.values(Icon).filter((v) => !isNaN(Number(v))).sort((n1,n2) => Number(n2)-Number(n1)).forEach((value) => {
+    if(badges.filter((v) => v==value).length > maxcount){
+      maxcount = badges.filter((v) => v==value).length;
+      result = Number(value);
+    }
+  });
+  return result;
+}
+
